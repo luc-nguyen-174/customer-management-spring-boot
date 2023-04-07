8e3108ca1c455a0f3588502571888bb153153f67
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-07 11:21:43 +0700

    first commit

---
+++ src/test/java/com/example/customermanagementspringboot/CustomerManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customermanagementspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
